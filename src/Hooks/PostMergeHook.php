99287124fbbca8d63d6c8b65adc1fe8bc1c180e2
Vlad Makin <vamakin@gmail.com>
2017-06-04 18:54:22 +0300

    add post-merge hook

---
+++ src/Hooks/PostMergeHook.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Vamakin\PhpPackagesSynchronizer\Hooks;
+
+class PostMergeHook implements Hook
+{
+    private $args;
+
+    public function __construct($args)
+    {
+        $this->args = $args;
+    }
+
+    public function getFiles()
+    {
+        $command = "git diff-tree -r --name-only --no-commit-id HEAD";
+        $output = [];
+        exec($command, $output);
+        return $output;
+    }
+}
