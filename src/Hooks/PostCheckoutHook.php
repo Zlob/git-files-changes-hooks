977fcf0fb7c24dbd03f375cfbaba69574728501a
Vlad Makin <vamakin@gmail.com>
2017-06-04 18:46:02 +0300

    bugfix

--- src/Hooks/PostCheckoutHook.php
+++ src/Hooks/PostCheckoutHook.php
@@ -16,7 +16,8 @@ class PostCheckoutHook implements Hook
         $commit_1 = $this->args[1];
         $commit_2 = $this->args[2];
         $command = "git diff-tree -r --name-only --no-commit-id $commit_1 $commit_2";
-        exec($command, $output = []);
+        $output = [];
+        exec($command, $output);
         return $output;
     }
 }
